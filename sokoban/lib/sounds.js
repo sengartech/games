aedda44530edd76d4edcf79a53e237f39d1473a0
sengartech <sengartech@gmail.com>
2025-02-14 14:34:37 +0530

    revert spell and sound-commented

--- sokoban/lib/sounds.js
+++ sokoban/lib/sounds.js
@@ -1,6 +1,7 @@
 const sounds = {
   new_level: new Audio("./assets/sounds/new_level.mp3"),
   move_click: new Audio("./assets/sounds/move_click.mp3"),
+  revert: new Audio("./assets/sounds/revert.mp3"),
   gem_collected: new Audio("./assets/sounds/gem_collected.mp3"),
   win: new Audio("./assets/sounds/win.mp3"),
 };
